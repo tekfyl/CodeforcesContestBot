c9543514917b39de41f925f6887ad2d818ca1d3e
Tushar Arora <chowki@Tushars-MacBook-Pro.local>
2020-09-26 17:00:14 +0530

    Added upper bound lower bound and few useful stls

--- template.cpp
+++ template.cpp
@@ -3,15 +3,21 @@
 using namespace std;
 
 #define gc getchar_unlocked
-#define fo(i,n) for(i=0;i<n;i++)
-#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define fo(i,n) for(auto i=0;i<n;i++)
+#define Fo(i,k,n) for(auto i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
 #define ll long long
 #define deb(x) cout << #x << "=" << x << endl
 #define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
 #define pb push_back
 #define mp make_pair
-#define F first
-#define S second
+#define ii pair <int, int>
+#define LL pair <ll, ll>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define lb lower_bound
+#define ub upper_bound
 #define all(x) x.begin(), x.end()
 #define clr(x) memset(x, 0, sizeof(x))
 #define sortall(x) sort(all(x))
@@ -60,23 +66,6 @@ vi g[N];
 
 /* ************************************************************************************************************** */
 
-void solve() {
-  int i, j, n, m, in, ans=0;
-}
-
-int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-
-    int t = 1;
-    cin >> t;
-    while(t--) {
-      solve();
-    }
-    
-    return 0;
-}
-
 int mpow(int base, int exp) {
   base %= mod;
   int result = 1;
@@ -109,4 +98,22 @@ void dfs(int u, int par){
 	}
   // parental stuff
 
+}
+
+void solve() {
+  ll j, n, m, in, ans=0;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+    
+    return 0;
 }
